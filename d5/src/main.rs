bff11415ecff0944374f134c1e6d75383c1fb33e
BEnh3N <9805092-BEnh3N@users.noreply.gitlab.com>
2023-12-20 01:18:35 -0800

    d5 part 2 complete

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -39,114 +39,36 @@ fn main() {
 
     let mut lowest_location = u64::MAX;
 
-    for seed in seeds {
-        println!("SEED: {}", seed);
-        let mut source = seed;
+    // Loop through every group of two in the parsed seed numbers (b/c part 2 is a b***)
+    for seed_pair in seeds.chunks_exact(2) {
+        let start = seed_pair[0];
+        let end = start + seed_pair[1];
 
-        for map in &mappings {
-            for mapping in map {
-                if source >= mapping.source_start && source < mapping.source_start + mapping.range {
-                    print!("{}", source);
-                    source = mapping.dest_start + (source - mapping.source_start);
-                    println!(" -> {}", source);
-                    break;
+        for seed in start..end {
+            // Source value always starts as the seed; mapped to each eventual destination
+            let mut source = seed;
+
+            // Loop through all mapping groups
+            for map in &mappings {
+                // Loop through each mapping in the mapping group, mapping as required
+                for mapping in map {
+                    // If the source falls within the mapping's source range, map it to the destination and break out
+                    if source >= mapping.source_start
+                        && source < mapping.source_start + mapping.range
+                    {
+                        source = mapping.dest_start + (source - mapping.source_start);
+                        break;
+                    }
                 }
             }
-        }
 
-        println!("----");
-
-        let location = source;
-        if location < lowest_location {
-            lowest_location = location;
+            // Check if the location is the lowest so far
+            let location = source;
+            if location < lowest_location {
+                lowest_location = location;
+            }
         }
     }
 
     println!("LOWEST LOCATION: {}", lowest_location);
-
-    // input.lines();
-
-    // let mut seeds = vec![];
-    // let mut mappings = vec![];
-
-    // let mut mapping_index = 0;
-    // let mut start_indices = vec![];
-
-    // Loop through all lines in input
-    // for (i, line) in input.lines().enumerate() {
-    //     // First line always contains seed info
-    //     if i == 0 {
-    //         // Parse first line for just seed nums
-    //         seeds = line
-    //             .split_once(':')
-    //             .unwrap()
-    //             .1
-    //             .split_whitespace()
-    //             .map(|x| x.parse::<u64>().unwrap())
-    //             .collect::<Vec<u64>>();
-    //     } else {
-    //         // All mapping lines start with a number; filter that way
-    //         if line.starts_with(char::is_numeric) {
-    //             // Take all nums from line and put them into a vector
-    //             let nums = line
-    //                 .split_whitespace()
-    //                 .map(|x| x.parse::<u64>().unwrap())
-    //                 .collect::<Vec<u64>>();
-    //             // Each number corresponds to a different part of the mapping
-    //             let map = Mapping {
-    //                 source_start: nums[1],
-    //                 dest_start: nums[0],
-    //                 range: nums[2],
-    //             };
-
-    //             mappings.push(map);
-    //             mapping_index += 1;
-    //         } else if line.starts_with(char::is_alphabetic) {
-    //             // Use labeled lines to find each starting index
-    //             start_indices.push(mapping_index);
-    //         }
-    //     }
-    // }
-
-    // start_indices.push(mapping_index);
-
-    // let mut fertilizers = vec![];
-
-    // Test each seed, seeing the path it takes down the mappings
-    // for seed in 0..1 {
-    //     let mut source = seeds[0]; // Initial source is the seed value; is later changed
-
-    //     for j in 0..start_indices.len() - 1 {
-    //         let start_i = start_indices[j];
-    //         let end_i = start_indices[j + 1];
-
-    //         let mut found = false;
-    //         let mut i = start_i;
-
-    //         while i < end_i && !found {
-    //             let mapping = &mappings[i];
-    //             if source >= mapping.source_start && source < mapping.source_start + mapping.range {
-    //                 source = source - mapping.source_start + mapping.dest_start;
-    //                 println!("FOUND! {:?}", mapping);
-    //                 found = true;
-    //             }
-    //             i += 1;
-    //         }
-
-    //         // for i in start_i..end_i {
-    //             let mapping = &mappings[i];
-    //             println!("SOURCE: {}", source);
-    //             if source >= mapping.source_start && source < mapping.source_start + mapping.range {
-    //                 source = source - mapping.source_start + mapping.dest_start;
-    //                 println!("{} Found in range: {:?}", source, mapping);
-    //                 break;
-    //             } else {
-    //                 println!("Not found... Trying next!");
-    //             }
-    //         // }
-    //         println!("GROUP DONE");
-    //     }
-
-    //     println!("SEED DONE");
-    // }
 }
