28b9fb1e9f41c99626d2f09c3d2bd6966c9e0a9f
BEnh3N <bhpohlmann@icloud.com>
2023-12-20 14:34:08 -0800

    d6 part 2 complete

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -4,27 +4,20 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let lines = input.lines().collect::<Vec<&str>>();
 
-    let times: Vec<u32> = lines[0].split_whitespace().filter_map(|x| x.parse().ok()).collect();
-    let distances: Vec<u32> = lines[1].split_whitespace().filter_map(|x| x.parse().ok()).collect();
+    let time: u64 = lines[0].chars().filter(|c| c.is_numeric()).collect::<String>().parse().unwrap();
+    let distance_to_beat: u64 = lines[1].chars().filter(|c| c.is_numeric()).collect::<String>().parse().unwrap();
 
-    let mut num_ways = 1;
+    println!("{}, {}", time, distance_to_beat);
 
-    for i in 0..times.len() {
-        let time = times[i];
-        let distance_to_beat = distances[i];
+    let mut count = 0;
 
-        let mut count = 0;
-
-        for time_held in 0..time {
-            let time_to_move = time - time_held;
-            let distance_traveled = time_to_move * time_held;
-            if distance_traveled > distance_to_beat {
-                count += 1;
-            }
+    for time_held in 0..time {
+        let time_to_move = time - time_held;
+        let distance_traveled = time_to_move * time_held;
+        if distance_traveled > distance_to_beat {
+            count += 1;
         }
-
-        num_ways *= count;
     }
 
-    println!("{}", num_ways);
+    println!("{}", count);
 }
